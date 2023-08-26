2974bf45cd659284248a33adf497d45d73a0f226
HEPHZIBAI <113971953+HEPHZIBAI@users.noreply.github.com>
2023-08-26 16:50:39 +0530

    Add files via upload

---
+++ 14 day Print Name.cpp
@@ -0,0 +1,16 @@
+/*
+https://www.codingninjas.com/studio/problems/print-name_9065122?challengeSlug=ninja-slayground&leftPanelTab=1
+*/
+
+
+
+#include <bits/stdc++.h> 
+void printName(string s){
+    // Write your code here.
+    int i=5;
+    while(i>0)
+    {
+        cout<<s<<"\n";
+        i--;
+    }
+}
