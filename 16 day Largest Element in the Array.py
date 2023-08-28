960d41fe961341fe8000cec8f5c8ecaa36cb38cf
HEPHZIBAI <113971953+HEPHZIBAI@users.noreply.github.com>
2023-08-28 09:36:02 +0530

    Add files via upload

---
+++ 16 day Largest Element in the Array.py
@@ -0,0 +1,15 @@
+'''
+https://www.codingninjas.com/studio/problems/largest-element-in-the-array_9065124?challengeSlug=ninja-slayground&leftPanelTab=1
+'''
+
+
+
+from sys import *
+from collections import *
+from math import *
+
+def largestElement(arr: [], n: int) -> int:
+
+    # Write your code from here.
+    return(max(arr))
+    pass
