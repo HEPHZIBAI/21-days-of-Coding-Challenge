d86162d81d4508d9e953a630f59bda591bb2b8bf
HEPHZIBAI <113971953+HEPHZIBAI@users.noreply.github.com>
2023-08-30 15:46:29 +0530

    Add files via upload

---
+++ 18 day Remove Duplicates from Sorted Array.py
@@ -0,0 +1,12 @@
+'''
+https://www.codingninjas.com/studio/problems/remove-duplicates-from-sorted-array_9065126?challengeSlug=ninja-slayground&leftPanelTab=1
+'''
+
+
+
+def removeDuplicates(arr,n):
+    # Write your code here.
+    a=set(arr)
+    a=list(a)
+    return len(a)
+    pass
