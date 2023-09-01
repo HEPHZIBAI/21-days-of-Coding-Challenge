1773caefd687a44f52dc0ce34ce8aea24cb82d74
HEPHZIBAI <113971953+HEPHZIBAI@users.noreply.github.com>
2023-09-01 16:41:40 +0530

    Add files via upload

---
+++ 20 day Sum Of Max And Min.py
@@ -0,0 +1,16 @@
+'''
+https://www.codingninjas.com/studio/problems/sum-of-max-and-min_9065128?challengeSlug=ninja-slayground&leftPanelTab=1
+'''
+
+
+
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+def sumOfMaxMin(arr):
+    # Write your code here
+    #print(arr)
+    return(max(arr)+min(arr))
+    pass
